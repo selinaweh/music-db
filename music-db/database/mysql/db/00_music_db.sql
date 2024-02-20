8d192e80af0a2b65f23eca3cac001e9de12c2f4c
selinaweh <selinaweh11@gmail.com>
2024-02-20 15:38:12 +0100

    created music db

---
+++ music-db/database/mysql/db/00_music_db.sql
@@ -0,0 +1,54 @@
+CREATE TABLE files (
+    file_id INT PRIMARY KEY,
+    file_data LONGBLOB,
+    file_type ENUM('mp3', 'wav', 'rar')
+);
+
+CREATE TABLE converted_files (
+    conversion_id INT PRIMARY KEY,
+    original_file_id INT,
+    file_data LONGBLOB,
+    file_type ENUM('wav', 'rar'),
+    FOREIGN KEY (original_file_id) REFERENCES Files(file_id)
+);
+
+CREATE TABLE artists (
+    artist_id INT PRIMARY KEY,
+    artist_name VARCHAR(100)
+);
+
+CREATE TABLE genres (
+    genre_id INT PRIMARY KEY,
+    genre_name VARCHAR(100)
+);
+
+CREATE TABLE albums (
+    album_id INT PRIMARY KEY,
+    album_name VARCHAR(100),
+    artist_id INT
+);
+
+CREATE TABLE songs (
+    song_id INT PRIMARY KEY,
+    album_id INT,
+    genre_id INT,
+    file_id INT,
+    duration INT,
+    file_path VARCHAR(255) NOT NULL,
+    bit_rate INT,
+    sample_rate INT,
+    song_title VARCHAR(100),
+    release_date DATE,
+    FOREIGN KEY (file_id) REFERENCES Files(file_id),
+    FOREIGN KEY (album_id) REFERENCES Albums(album_id),
+    FOREIGN KEY (genre_id) REFERENCES Genres(genre_id)
+);
+
+CREATE TABLE songs_artists (
+    song_id INT,
+    artist_id INT,
+    PRIMARY KEY (song_id, artist_id),
+    FOREIGN KEY (song_id) REFERENCES Songs(song_id),
+    FOREIGN KEY (artist_id) REFERENCES Authors(artist_id)
+);
+
